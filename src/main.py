f3f43e99ff5eaa0efb3f032a0d8b9ddb8869133a
Jared Lincenberg <jaredlincenberg@mymail.mines.edu>
2026-07-09 13:23:00 -0600

    Updates to main.

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ import json
 import pandas as pd
 import requests_cache      
 import numpy as np
-# requests_cache.install_cache('cache/inaturalist_cache', backend='sqlite', expire_after=1800)
+requests_cache.install_cache('cache/inaturalist_cache', backend='sqlite', expire_after=1800000)
 
 INAT_COLORADO_ID = 34
 INAT_USA_ID = 1
@@ -27,6 +27,10 @@ INAT_COUNTY_DATA = None
 TAXONOMY_DATA = None
 COUNTY_SPECIES = None
 
+REQUEST_COUNT = 0
+CACHE_HIT_COUNT = 0
+CACHE_MISS_COUNT = 0
+
 #### Get the counties for a given state ID from the iNaturalist places CSV file
 def get_USA_State_Counties(id):
     counties = []
@@ -39,33 +43,35 @@ def get_USA_State_Counties(id):
 
 ### Call the iNaturalist API to get the species counts for a given county ID
 def get_County_Species_Counts(county_id, rank='species', acc_below="1000", page=1, per_page=500, native=None, invasive=None,endemic=None,threatened=None, captive="false", identified="true", photos="true", verifiable="true", quality_grade="research"):
-
-    params = {
-        "verifiable": "true",
-        "captive":  captive,
-        "identified": identified,
-        "place_id": str(county_id),
-        "quality_grade": quality_grade,
-        "order": "desc",
-        "order_by": "count",
-        "rank": rank,
-        "acc_below": acc_below,
-        "page": page,
-        "per_page": per_page,
-        "photos": photos,
-        "fields": "all" #"(taxon:(name:!t,rank:!t))",
-            }
+    # List of Tuples, to keep the order of the parameters consistent
+    params = [
+        ("verifiable", "true"),
+        ("captive", captive),
+        ("identified", identified),
+        ("place_id", str(county_id)),
+        ("quality_grade", quality_grade),
+        ("order", "desc"),
+        ("order_by", "count"),
+        ("rank", rank),
+        ("acc_below", acc_below),
+        ("page", page),
+        ("per_page", per_page),
+        ("photos", photos),
+        ("fields", "all")  # "(taxon:(name:!t,rank:!t))",
+    ]
     if native is not None:
-        params["native"] = 'true' if native else 'false'
+        params.append(("native", 'true' if native else 'false'))
     if invasive is not None:
-        params["native"] = 'false' if invasive else 'true'
+        params.append(("native", 'false' if invasive else 'true'))
     if endemic is not None:
-        params["endemic"] = 'true' if endemic else 'false'
+        params.append(("endemic", 'true' if endemic else 'false'))
     if threatened is not None:
-        params["threatened"] = threatened
+        params.append(("threatened", threatened))
     try:
         response = requests.get("https://api.inaturalist.org/v2/observations/species_counts", params=params)
-        print(response.url) 
+        global REQUEST_COUNT
+        REQUEST_COUNT += 1
+        print(f"Request {REQUEST_COUNT}: {response.url}")
     except requests.exceptions.RequestException as e:
             print(f"Error occurred while making the request: {e}")
             return None
@@ -84,7 +90,15 @@ def get_county_geometry():
     global COUNTY_GEOJSON
 
     if COUNTY_GEOJSON is None:
-        response = requests.get("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Census_Counties/FeatureServer/0/query?where=STATE_ABBR%3D%27CO%27&outFields=*&returnGeometry=true&outSR=4326&f=geojson")
+        # List of Tuples, to keep the order of the parameters consistent
+        params = [
+            ("where", "STATE_ABBR='CO'"),
+            ("outFields", "*"),
+            ("returnGeometry", "true"),
+            ("outSR", "4326"),
+            ("f", "geojson")
+        ]
+        response = requests.get("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Census_Counties/FeatureServer/0/query", params=params)
         COUNTY_GEOJSON = response
     return COUNTY_GEOJSON
 
@@ -92,7 +106,14 @@ def get_county_geometry():
 def get_county_properties():
     global COUNTY_PROPERTIES
     if COUNTY_PROPERTIES is None:
-        response = requests.get("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Census_Counties/FeatureServer/0/query?where=STATE_ABBR%3D%27CO%27&outFields=*&returnGeometry=false&outSR=4326&f=geojson")
+        params = {
+            "where": "STATE_ABBR='CO'",
+            "outFields": "*",
+            "returnGeometry": "false",
+            "outSR": "4326",
+            "f": "geojson"
+        }  
+        response = requests.get("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Census_Counties/FeatureServer/0/query", params=params)
         COUNTY_PROPERTIES = response
     return COUNTY_PROPERTIES
 
@@ -102,8 +123,8 @@ def get_county_species_dataFrame():
         COUNTY_SPECIES = pd.DataFrame(columns=['county_id','taxon_id'])
         COUNTY_SPECIES.set_index(['county_id','taxon_id'], inplace=True)
     return COUNTY_SPECIES
-def get_county_species_result(county_id, filter_type="all", force_refresh=False):
-    print(filter_type)
+def get_county_species_result(county_id,county_name, filter_type="all", force_refresh=False):
+    print(filter_type, county_id, county_name)
     global COUNTY_SPECIES
     county_species_df = get_county_species_dataFrame()
     total_pages = 1
@@ -124,53 +145,75 @@ def get_county_species_result(county_id, filter_type="all", force_refresh=False)
         json_response = response.json()
         if(response is None):
             return
-        print(json_response['total_results'])
-        print(json_response['total_results'] // per_page + (1 if json_response['total_results'] % per_page > 0 else 0))
-        
         total_pages = json_response['total_results'] // per_page + (1 if json_response['total_results'] % per_page > 0 else 0)
         current_page += 1
         
         results = json_response['results']
         
         row = []
-        print(filter_type+"_species_count")
+        # print(filter_type+"_species_count")
         if filter_type+"_species_count" not in county_species_df.columns:
             county_species_df[filter_type+"_species_count"] = np.nan
 
         for result in results:
             # print(county_species_df.head())
             county_species_df.loc[(county_id, result['taxon']['id']), filter_type+"_species_count"] = int(result['count'])
-            # row.append({'county_id': county_id, 
-            #             'taxonID': result['taxon']['id'],
-            #             filter_type + "_species_count": result['count']
-            #             })
-        # county_species_df = pd.DataFrame.from_records(row)
-        # COUNTY_SPECIES.county_species_df
-        sleep(1)
-    # append({'county_id': county_id, 'species_count': results['count'], 'preferred_establishment_means': results['preferred_establishment_means']}, ignore_index=True)
+            county_species_df.loc[(county_id, result['taxon']['id']), "county_name"] = county_name
+        if not response.from_cache:
+            global CACHE_MISS_COUNT 
+            CACHE_MISS_COUNT += 1
+            print("From API")
+            sleep(1)
+        else:
+            global CACHE_HIT_COUNT
+            CACHE_HIT_COUNT += 1
+            print("From Cache")
 
 
 def add_county_species_counts_to_dataframe(df, force_refresh=False):
     df = get_county_dataframe(force_refresh=force_refresh)
+    yearMonthDay = datetime.now().strftime("%Y-%m-%d")
+    if os.path.exists("data/processed/colorado_county_species_counts_"+yearMonthDay+".parquet") and not force_refresh:
+        print("Loading county species counts from parquet file")
+        df_county_species_df = pd.read_parquet("data/processed/colorado_county_species_counts_"+yearMonthDay+".parquet")
+    else:
+        print("Getting county species counts from iNaturalist API")
+        df_county_species_df = get_county_species_dataFrame()
+        # df_county_species_df = pd.DataFrame(columns=['county_id','taxon_id'])
+        # df_county_species_df.set_index(['county_id','taxon_id'], inplace=True)
+        # df = get_county_dataframe(force_refresh=force_refresh)
+        for df_row in df.itertuples():
+            get_county_species_result(county_id=df_row.id, county_name=df_row.county_name, filter_type="all")
+            get_county_species_result(county_id=df_row.id, county_name=df_row.county_name, filter_type="native")
+            get_county_species_result(county_id=df_row.id, county_name=df_row.county_name, filter_type="invasive")
+            get_county_species_result(county_id=df_row.id, county_name=df_row.county_name, filter_type="endemic")
+        df_county_species_df = get_county_species_dataFrame()
+        
+        yearMonthDay = datetime.now().strftime("%Y-%m-%d")
+        df_county_species_df.to_parquet("data/processed/colorado_county_species_counts_"+yearMonthDay+".parquet", index=True)
+        print("Finished getting species counts for all counties. Saving to parquet file")
+    
+    # df_county_species_df.reset_index(inplace=True)
+    # df_county_species_df = df_county_species_df[df_county_species_df['all_species_count'] > 2]
+    print("df_county_species_df head:", df_county_species_df.head())
+    df_county = df_county_species_df.groupby('county_id').agg({
+        'all_species_count': 'count',
+        'native_species_count': 'count',
+        'invasive_species_count': 'count',
+        'endemic_species_count': 'count',
+        'county_name': 'first'
+    })
+    df_county = df_county.assign(native_invasive_ratio=lambda x: x['native_species_count'] / x['invasive_species_count'].replace(0, np.nan))
+    return df_county
+def add_county_species_counts_to_dataframe_old(df, force_refresh=False):
+    df = get_county_dataframe(force_refresh=force_refresh)
 
     def get_species_count(row):
             try:
-                get_county_species_result(county_id=row['id'], filter_type="all")
-                sleep(1.5)  # Sleep for 1 second to avoid hitting the API rate limit
-                get_county_species_result(county_id=row['id'], filter_type="native")
-                sleep(1.5)  # Sleep for 1 second to avoid hitting the API rate limit
-                get_county_species_result(county_id=row['id'], filter_type="invasive")
-                sleep(1.5)  # Sleep for 1 second to avoid hitting the API rate limit
-                get_county_species_result(county_id=row['id'], filter_type="endemic")
-
                 species_counts = get_County_Species_Counts(county_id=row['id']).json()
+                # print(species_counts)
+                sleep(1.5)  # Sleep for 1.5 second to avoid hitting the API rate limit
 
-                print(species_counts)
-                sleep(1.5)  # Sleep for 1 second to avoid hitting the API rate limit
-                # county_species_df = get_county_species_dataFrame()
-                # county_species_df = pd.concat([county_species_df, pd.DataFrame({'county_id': [row['id']], 'species_count': [species_counts["total_results"]]})], ignore_index=True)
-
-                # print(row['display_name'], species_counts["total_results"])
                 return int(species_counts["total_results"])
             except Exception as e:
                 # print(row)
@@ -261,18 +304,22 @@ def main():
     print("Got counties shape data, now plotting test")
     
     df = get_county_dataframe()
-    df = add_county_species_counts_to_dataframe(df, force_refresh=True)
+    df = add_county_species_counts_to_dataframe(df, force_refresh=False)
     print(df.head())
-    df = get_species_density()
+    # df = get_species_density()
     import plotly.express as px
     print("Got counties data, now plotting Species Counts")
-    print(df['species_count'].head())
-    fig = px.choropleth_map(df, geojson=get_county_geometry().json(),featureidkey='properties.NAME', locations='county_name', color='species_count',
+    print(df['invasive_species_count'].head())
+    print(f'requests: {REQUEST_COUNT}, Cache Hits: {CACHE_HIT_COUNT}, Cache Misses: {CACHE_MISS_COUNT}')
+
+    fig = px.choropleth_map(df, geojson=get_county_geometry().json(),featureidkey='properties.NAME', locations='county_name', color='native_invasive_ratio',
                             color_continuous_scale="Viridis",
                             #range_color=(0, 12),
                             map_style="carto-positron",
                             zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
                             opacity=0.5,
+                            hover_data=['county_name','all_species_count','native_species_count','invasive_species_count','endemic_species_count','native_invasive_ratio'],
+                            
                             labels={'species_per_person_demoniator_log':'Species Per Person D (Log)','species_per_person_log':'Species Per Person (Log)','species_per_person':'Species Per Person','species_density':'Species Density','species_density_per_sq_mi':'Species Density per Sq Mi','species_density_log':'Species Density (Log) '}
                             )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
